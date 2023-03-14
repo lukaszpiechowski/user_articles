ee778aa6e8f7b25033fca6d23a56d4a48622be4b
Łukasz Piechowski <ukaasz0@gmail.com>
2023-03-14 12:32:35 +0100

    test_repository

---
+++ test/models/author_model_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import 'package:user_articles/domain/models/author_model.dart';
+
+void main() {
+  test('cp tak naprawde sie wysypalo', () {
+    // 1
+    final model = AuthorModel(1, '', 'Kamil', 'Kowalski');
+
+    // 2
+    final result = model.name;
+
+    // 3
+    expect(result, 'Kamil Kowalski');
+  });
+}
